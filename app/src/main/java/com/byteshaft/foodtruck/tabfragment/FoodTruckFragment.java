8cc549abac71c6ca49b6277afcfe59fb40620489
s9iper1 <s9iper1@gmail.com>
2017-01-12 16:34:33 +0500

    Ui changes

--- app/src/main/java/com/byteshaft/foodtruck/tabfragment/FoodTruckFragment.java
+++ app/src/main/java/com/byteshaft/foodtruck/tabfragment/FoodTruckFragment.java
@@ -3,6 +3,8 @@ package com.byteshaft.foodtruck.tabfragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +19,14 @@ public class FoodTruckFragment extends Fragment {
 
     private View mBaseView;
 
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (!((AppCompatActivity) getActivity()).getSupportActionBar().isShowing()) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+        }
         mBaseView = inflater.inflate(R.layout.truck_fragment, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Food Trucks");
+
         return mBaseView;
     }
 }
--- app/src/main/java/com/byteshaft/foodtruck/tabfragment/SearchFragment.java
+++ app/src/main/java/com/byteshaft/foodtruck/tabfragment/SearchFragment.java
@@ -1,8 +1,8 @@
 package com.byteshaft.foodtruck.tabfragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +17,9 @@ public class SearchFragment extends Fragment {
 
     private View mBaseView;
 
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         mBaseView = inflater.inflate(R.layout.search_layout, container, false);
         return mBaseView;
     }
