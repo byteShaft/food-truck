7187e9e70f8fdb632213ac099895d7d386e86874
rb-shahid <shgr8rb@gmail.com>
2017-01-14 22:31:26 +0500

    add login and registration and add codeConfirmation UI

--- app/src/main/java/com/byteshaft/foodtruck/accounts/RegisterActivity.java
+++ app/src/main/java/com/byteshaft/foodtruck/accounts/RegisterActivity.java
@@ -1,17 +1,27 @@
 package com.byteshaft.foodtruck.accounts;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.byteshaft.foodtruck.MainActivity;
 import com.byteshaft.foodtruck.R;
-import com.github.siyamed.shapeimageview.CircularImageView;
+import com.byteshaft.foodtruck.utils.AppGlobals;
+import com.byteshaft.foodtruck.utils.Helpers;
+import com.byteshaft.requests.HttpRequest;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener,
+        HttpRequest.OnReadyStateChangeListener, HttpRequest.OnErrorListener {
 
     private Button mRegisterButton;
     private EditText mUsername;
@@ -24,7 +34,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private String mVerifyPasswordString;
     private String mPasswordString;
 
-    private static final int SELECT_PICTURE = 1;
+
+    private HttpRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +53,9 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.register_button:
-                validateEditText();
+                if (validateEditText()) {
+                    registerUser(mUsernameString, mEmailAddressString, mPasswordString);
+                }
         }
     }
 
@@ -81,5 +94,70 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             mEmailAddress.setError(null);
         }
         return valid;
+    }
+
+    private void registerUser(String username, String email, String password) {
+        request = new HttpRequest(getApplicationContext());
+        request.setOnReadyStateChangeListener(this);
+        request.setOnErrorListener(this);
+        request.open("POST", String.format("%suser/register", AppGlobals.BASE_URL));
+        request.send(getRegisterData(username, password, email));
+        Helpers.showProgressDialog(RegisterActivity.this, "Registering User ");
+    }
+
+    private String getRegisterData(String username, String password, String email) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("full_name", username);
+            jsonObject.put("email", email);
+            jsonObject.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+
+    }
+
+    @Override
+    public void onReadyStateChange(HttpRequest request, int readyState) {
+        switch (readyState) {
+            case HttpRequest.STATE_DONE:
+                Helpers.dismissProgressDialog();
+                Log.i("Tag", "Response " + request.getResponseText());
+                switch (request.getStatus()) {
+                    case HttpRequest.ERROR_NETWORK_UNREACHABLE:
+                        AppGlobals.alertDialog(this, "Registration Failed!",
+                                "Please check your internet connection");
+                        break;
+                    case HttpURLConnection.HTTP_BAD_REQUEST:
+                        AppGlobals.alertDialog(RegisterActivity.this, "Registration Failed!",
+                                "Email already in use");
+                        break;
+                    case HttpURLConnection.HTTP_CREATED:
+                        Toast.makeText(getApplicationContext(), "Activation code has been sent " +
+                                "to your Email Address", Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jsonObject = new JSONObject(request.getResponseText());
+                            String username = jsonObject.getString(AppGlobals.KEY_FULL_NAME);
+                            String email = jsonObject.getString(AppGlobals.KEY_EMAIL);
+                            String id = jsonObject.getString(AppGlobals.KEY_USER_ID);
+                            // saving details
+                            AppGlobals.saveDataToSharedPreferences(AppGlobals.KEY_FULL_NAME, username);
+                            AppGlobals.saveDataToSharedPreferences(AppGlobals.KEY_EMAIL, email);
+                            AppGlobals.saveDataToSharedPreferences(AppGlobals.KEY_USER_ID, id);
+                            AppGlobals.saveUserLogin(true);
+                            LoginActivity.getInstance().finish();
+                            finish();
+                            startActivity(new Intent(this, MainActivity.class));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                }
+        }
+    }
+
+    @Override
+    public void onError(HttpRequest request, int readyState, short error, Exception exception) {
+
     }
 }
